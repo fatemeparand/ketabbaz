4f8e055c9b4230dd2dee47ab1aaddf7bd9059379
fateme_parand <fateme.parand94@gmail.com>
2022-12-02 18:30:38 +0330

    create bookblog model and show in the admin panel

--- book_blog/admin.py
+++ book_blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import BookBlog
 
-# Register your models here.
+
+@admin.register(BookBlog)
+class BookBlogAdmin(admin.ModelAdmin):
+    list_display = ('book_name', 'book_author', 'author', 'status')
