8689e74dff6cc915ab87f8365c2c55f3abcf95ce
fateme_parand <fateme.parand94@gmail.com>
2022-12-07 13:33:21 +0330

    Implementation change password

--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
     path('accounts/', include('allauth.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('book/', include('book_store.urls')),
 
     # rosetta(i18n)
