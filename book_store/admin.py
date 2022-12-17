09b270b4951ef780e1ff38cb83099c08d5e45073
fateme_parand <fateme.parand94@gmail.com>
2022-12-17 14:17:43 +0330

    show comments next to book details in the admin panel with tabularinline

--- book_store/admin.py
+++ book_store/admin.py
@@ -4,14 +4,21 @@ from .models import Book, Subject, Comment
 admin.site.register(Subject)
 
 
+class CommentsInline(admin.TabularInline):
+    model = Comment
+    list_display = ['book', 'author', 'score', 'datetime_created', 'active']
+    ordering = ('-datetime_created',)
+    extra = 1
+
+
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('book_name', 'book_author', 'author', 'status', 'datetime_modified', 'active')
     ordering = ('-datetime_modified',)
+    inlines = [CommentsInline]
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ['book', 'author', 'score', 'datetime_created', 'active']
     ordering = ('-datetime_created',)
-
