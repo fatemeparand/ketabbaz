9400b6a719f660408f56f9ca50b36529c56af901
fateme_parand <fateme.parand94@gmail.com>
2023-01-01 20:55:01 +0330

    create orderitem model

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('user', 'is_paid', 'first_name', 'last_name', 'datetime_modified',)
+    ordering = ('-datetime_modified',)
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('order', 'book', 'quantity', 'price', )
+
--- orders/models.py
+++ orders/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
 
+from book_store.models import Book
+
 
 class Order(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, verbose_name=_('user'))
@@ -19,3 +21,15 @@ class Order(models.Model):
 
     def __str__(self):
         return f' order{self.id}'
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items', verbose_name=_('order'))
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='order_items', verbose_name=_('book'))
+
+    quantity = models.PositiveIntegerField(default=1, verbose_name=_('book quantity'))
+    price = models.PositiveIntegerField(verbose_name=_('book price'))
+
+    def __str__(self):
+        return f'OrderItem {self.id}: {self.book} x {self.price}'
+
