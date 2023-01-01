9268642db93685ed57a99ae9294d00c7603f8ee2
fateme_parand <fateme.parand94@gmail.com>
2023-01-02 01:36:44 +0330

    create order form and use it in order_create_detail page

--- orders/forms.py
+++ orders/forms.py
@@ -2,4 +2,7 @@ from django import forms
 from .models import Order
 
 
-
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ['first_name', 'last_name', 'phone_number', 'address', 'order_notes']
--- orders/models.py
+++ orders/models.py
@@ -8,13 +8,13 @@ from book_store.models import Book
 class Order(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, verbose_name=_('user'))
 
-    is_paid = models.BooleanField(default=True)
+    is_paid = models.BooleanField(default=False)
 
     first_name = models.CharField(max_length=100, verbose_name=_('name'))
     last_name = models.CharField(max_length=100, verbose_name=_('family'))
     phone_number = models.CharField(max_length=11, verbose_name=_('phone number'))
     address = models.CharField(max_length=700, verbose_name=_('address'))
-    order_notes = models.CharField(max_length=700, verbose_name=_('note'))
+    order_notes = models.CharField(max_length=700, blank=True, verbose_name=_('note'))
 
     datetime_created = models.DateTimeField(auto_now=True, verbose_name=_('time create'))
     datetime_modified = models.DateTimeField(auto_now=True, verbose_name=_('time modified'))
